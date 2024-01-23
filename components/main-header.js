1e792b7c88417fd815c28e0778b4a7c198136e1f
rishavConsoleLog21 <kumarrishavcool21@gmail.com>
2024-01-23 19:52:12 +0530

    Update layout and add main header comp with navbar

---
+++ components/main-header.js
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import logoImg from "@/assets/logo.png";
+import styles from "./main-header.module.css";
+
+export default function MainHeader() {
+  return (
+    <header className={styles.header}>
+      <Link className={styles.logo} href="/">
+        <img src={logoImg.src} alt="A plate with food on it" />
+        NextLevel Food
+      </Link>
+
+      <nav className={styles.nav}>
+        <ul>
+          <li>
+            <Link href="/meals">Browse Meals</Link>
+            <Link href="/community">Community</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
