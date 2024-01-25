a5290c8a788ce2e77423a8c8418f1c148b7bc8d5
rishavConsoleLog21 <kumarrishavcool21@gmail.com>
2024-01-25 15:20:55 +0530

    + error handling and form validation to ShareMealPage

---
+++ app/meals/share/error.js
@@ -0,0 +1,10 @@
+"use client";
+
+export default function ErrorPage() {
+  return (
+    <main className="error">
+      <h1>Invalid Input!</h1>
+      <p>Failed to create meal.</p>
+    </main>
+  );
+}
